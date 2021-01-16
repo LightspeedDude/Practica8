6b23c0e93c535ac8c9e3c92bc0016548e6758292
Julio <julio_martinez25@live.com>
2021-01-15 22:50:35 -0300

    Primer commit

---
+++ Persistenciamutiplicativatest.lpr
@@ -0,0 +1,26 @@
+program Persistenciamutiplicativatest;
+
+{$mode objfpc}{$H+}
+
+uses
+  Classes, consoletestrunner, persistenciamultiplicativatestcase;
+
+type
+
+  { TMyTestRunner }
+
+  TMyTestRunner = class(TTestRunner)
+  protected
+  // override the protected methods of TTestRunner to customize its behavior
+  end;
+
+var
+  Application: TMyTestRunner;
+
+begin
+  Application := TMyTestRunner.Create(nil);
+  Application.Initialize;
+  Application.Title := 'FPCUnit Console test runner';
+  Application.Run;
+  Application.Free;
+end.
---
+++ persistenciamultiplicativatestcase.pas
@@ -0,0 +1,48 @@
+unit persistenciamultiplicativatestcase;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, fpcunit, testutils, testregistry;
+
+type
+
+  TTestCase1= class(TTestCase)
+  published
+    procedure TestOne;
+    procedure TestTwo;
+    procedure TestThree;
+    procedure TestFour;
+    procedure TestFive;
+  end;
+
+implementation
+
+procedure TTestCase1.TestOne;
+begin
+  Check(39 = 3, 'Prueba uno');
+end;
+procedure TTestCase1.TestTwo;
+begin
+    Check(325 = 2, 'Prueba 2');
+end;
+procedure TTestCase1.TestThree;
+begin
+    Check(123 = 1,'Prueba 3');
+end;
+procedure TTestCase1.TestFour;
+begin
+    Check(6874 = 4 ,'Prueba 4');
+end;
+procedure TTestCase1.TestFive;
+begin
+    Check(986 = 3, 'Prueba 5');
+end;
+
+initialization
+
+  RegisterTest(TTestCase1);
+end.
+
